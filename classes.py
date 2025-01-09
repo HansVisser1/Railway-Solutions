b7935ab4e9875081c187b18c62688efd74c4944b
LeanderGall <leanderg98@gmail.com>
2025-01-09 14:26:55 +0100

    updated classes

--- classes.py
+++ classes.py
@@ -68,8 +68,3 @@ class Traject():
         for connection in self.connections:
             self.time += int(connection[2])
         return self.time
-
-traject1 = Traject()
-traject1.run('StationsHolland.csv', 'ConnectiesHolland.csv')
-print(traject1.connections)
-print(traject1.time)
