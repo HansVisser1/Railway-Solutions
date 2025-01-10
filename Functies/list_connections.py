a556e44a859d857ade7df99de7d17f792f7af70d
LeanderGall <leanderg98@gmail.com>
2025-01-10 13:41:04 +0100

    updated list connections

--- Functies/list_connections.py
+++ Functies/list_connections.py
@@ -1,6 +1,9 @@
 from Functies.classes import Traject
 
 def list_connections(traject_list):
+    """
+    This function takes the connections for each trajects and appends them to a list, which is then returned.
+    """
     connections_list = []
     for traject in traject_list:
         connections_list.append(traject.connections)
