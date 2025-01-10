9f46c17360126bcaf74e818da506a807102a0588
LeanderGall <leanderg98@gmail.com>
2025-01-10 12:41:50 +0100

    main aangepast

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Functies.read_files import read_data
 from Functies.random_multiple_trajects import random_multiple_trajects
 import Visualisation.visualize_railway
 
-trajects, cost = random_multiple_trajects(1, 7)
+nr, trajects, cost = random_multiple_trajects(1, 7)
 print(f"cost: {cost}")
 for traject in trajects:
     print(traject.connections)
