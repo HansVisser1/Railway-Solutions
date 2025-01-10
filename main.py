f9164d6139f7e4010e7732e6b36bddd900f1bd88
LeanderGall <leanderg98@gmail.com>
2025-01-10 14:01:36 +0100

    main updated

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from Functies.read_files import read_data
 from Functies.random_multiple_trajects import random_multiple_trajects
 from Functies.baseline import baseline
 from Functies.list_connections import list_connections
-import Visualisation.visualize_railway
+from Visualisation.visualize_railway import visualize_traject
 
+# create random number of trajects and return the nr of trajects, trajects objects list and the total cost
 nr, trajects, cost = random_multiple_trajects(1, 7)
+
+# make the connections_list for the visualization
 connections_list = list_connections(trajects)
 
-#print(baseline(100))
+# read the data for the visualization
+station_dict, connection_dict = read_data('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
+
+# visualize the trajects
+visualize_traject(station_dict, trajects)
+
+# baseline cost calculation
+print(baseline(100))
