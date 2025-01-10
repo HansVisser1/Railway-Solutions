93b1c8e4195b4910948ab8b5299bf9e7d6cae527
LeanderGall <leanderg98@gmail.com>
2025-01-10 11:31:33 +0100

    multiple trajects & random multiple trajects functies gemaakt en main aangepast

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pprint
 from Functies.classes import Traject
 from Functies.read_files import read_data
+from Functies.random_multiple_trajects import random_multiple_trajects
+
 import Visualisation.visualize_railway
 
 
-traject1 = Traject()
-traject1.run('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
-print(traject1.connections)
-print(traject1.time)
+trajects = random_multiple_trajects(1, 7)
+for traject in trajects:
+    print(traject.connections)
+    print()
 
-score = traject1.calculate_score()
-print(f"Trajectory Score: {score}")
 
-stations, connections = read_data('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
+
+
+# traject1 = Traject()
+# traject1.run('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
+# print(traject1.connections)
+# print(traject1.time)
+#
+# score = traject1.calculate_score()
+# print(f"Trajectory Score: {score}")
+#
+# stations, connections = read_data('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
 # pprint.pprint(stations)
 # pprint.pprint(connections)
