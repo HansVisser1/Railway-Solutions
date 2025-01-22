c3819b0f01c696bde3ea79b4885e7492572b1136
Victor-Brouns <vmbrouns@gmail.com>
2025-01-22 01:18:46 +0100

    Implemented visualisation and saving of multiple algorithm outputs. Improved main visualisation function.

--- Visualisation/visualize_railway.py
+++ Visualisation/visualize_railway.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 import geopandas as gpd
 
-def visualize_area(plt_axis, province):
+def visualize_area(plt_axis, province, algorithm):
     """
     Function uses a GeoJSON file (not received as argument) and plots the outline of the area the trains will operate in.
     The argument plt_axis is the shared axis between all matplotlib plots/figures. Passing ensures plotting in the same figure.
@@ -22,12 +22,12 @@ def visualize_area(plt_axis, province):
         provinces = ["Noord-Holland", "Zuid-Holland"]
         province_geo_data = geo_data[geo_data["name"].isin(provinces)]
         province_geo_data.plot(ax = plt_axis, edgecolor = 'grey', facecolor = 'none', zorder = 0)
-        plt_axis.set_title('Stations Noord- and Zuid-Holland')
+        plt_axis.set_title(f'Stations Noord- and Zuid-Holland ({algorithm})')
 
     # plotting the country borders
     else:
         geo_data.plot(ax = plt_axis, edgecolor = 'grey', facecolor = 'none', zorder = 0)
-        plt_axis.set_title('Stations Netherlands')
+        plt_axis.set_title(f'Stations Netherlands ({algorithm})')
 
 def visualize_stations(plt_axis, station_dict, province):
     """
@@ -68,12 +68,12 @@ def visualize_stations(plt_axis, station_dict, province):
     # naming the stations at their coordinates using name list
     for name, x, y in zip(station_names, x_coordinates, y_coordinates):
         if name in va_dict or name in ha_dict:
-            pass
-            plt_axis.text(x, y, name, fontsize = 8, ha = ha_dict[name], va = va_dict[name], color = 'black')
-        elif name not in va_dict and not province:
-            pass # TODO REMOVE
-            # plt_axis.text(x, y, name, fontsize = 8, ha = 'right', va = 'bottom', color = 'black')
+            plt_axis.text(x, y, name, fontsize = 9, ha = ha_dict[name], va = va_dict[name], color = 'black')
 
+        elif name not in va_dict and not province:
+            pass
+        else:
+            plt_axis.text(x, y, name, fontsize = 9, ha = 'right', va = 'bottom', color = 'black')
 
 def visualize_connections(plt_axis, station_dictionary):
     """
@@ -153,7 +153,7 @@ def visualize_traject(plt_axis, station_dictionary, traject_list):
                 plt.pause(0.25)
 
 
-def visualize_all_trajects(dict_stations, traject_list, province = True):
+def visualize_all_trajects(dict_stations, traject_list, file_name = None, province = True, save_figure = False, algorithm = 'Test'):
     """
     Function takes dictionary of stations produced by the read_data function and a traject list produced by
     the list_connections function.
@@ -161,13 +161,17 @@ def visualize_all_trajects(dict_stations, traject_list, province = True):
     The order of the visualize functions is of importance.
     """
     # defining figure and figure axis
-    fig, plt_axis = plt.subplots(figsize=(8, 8))
+    fig, plt_axis = plt.subplots(figsize=(10, 10))
 
     # calling the three visualisation functions
-    visualize_area(plt_axis, province)
+    visualize_area(plt_axis, province, algorithm)
     visualize_stations(plt_axis, dict_stations, province)
     visualize_connections(plt_axis, dict_stations)
     visualize_traject(plt_axis, dict_stations, traject_list)
+
+    if save_figure:
+        # saving figure as png image
+        plt.savefig(file_name, dpi = 600, bbox_inches = 'tight')
 
     # displaying the full final visual/graph
     plt.show()
@@ -236,6 +240,54 @@ if __name__ == "__main__":
                       ['Heemstede-Aerdenhout', 'Haarlem', '6'],
                       ['Haarlem', 'Amsterdam Sloterdijk', '11']]]
 
+    traject_depthfirst = [[['Dordrecht', 'Rotterdam Centraal', 0], ['Rotterdam Centraal', 'Schiedam Centrum', 0],
+        ['Schiedam Centrum', 'Delft', 0], ['Delft', 'Den Haag Centraal', 0], ['Den Haag Centraal', 'Leiden Centraal', 0],
+        ['Leiden Centraal', 'Heemstede-Aerdenhout', 0], ['Heemstede-Aerdenhout', 'Haarlem', 0], ['Haarlem', 'Amsterdam Sloterdijk', 0],
+        ['Amsterdam Sloterdijk', 'Amsterdam Centraal', 0], ['Amsterdam Centraal', 'Amsterdam Amstel', 0], ['Amsterdam Amstel', 'Amsterdam Zuid', 0],
+        ['Amsterdam Zuid', 'Schiphol Airport', 0], ['Schiphol Airport', 'Amsterdam Zuid', 0]], [['Zaandam', 'Beverwijk', 0], ['Beverwijk', 'Castricum', 0],
+        ['Castricum', 'Zaandam', 0], ['Zaandam', 'Beverwijk', 0], ['Beverwijk', 'Castricum', 0], ['Castricum', 'Alkmaar', 0], ['Alkmaar', 'Castricum', 0],
+        ['Castricum', 'Zaandam', 0]], [['Rotterdam Alexander', 'Gouda', 0], ['Gouda', 'Alphen a/d Rijn', 0], ['Alphen a/d Rijn', 'Leiden Centraal', 0],
+        ['Leiden Centraal', 'Schiphol Airport', 0], ['Schiphol Airport', 'Amsterdam Zuid', 0], ['Amsterdam Zuid', 'Amsterdam Sloterdijk', 0],
+        ['Amsterdam Sloterdijk', 'Zaandam', 0], ['Zaandam', 'Hoorn', 0]], [['Den Helder', 'Alkmaar', 0], ['Alkmaar', 'Hoorn', 0], ['Hoorn', 'Zaandam', 0],
+        ['Zaandam', 'Amsterdam Sloterdijk', 0], ['Amsterdam Sloterdijk', 'Haarlem', 0], ['Haarlem', 'Beverwijk', 0]],
+        [['Schiphol Airport', 'Leiden Centraal', 0], ['Leiden Centraal', 'Den Haag Centraal', 0], ['Den Haag Centraal', 'Gouda', 0],
+        ['Gouda', 'Rotterdam Alexander', 0], ['Rotterdam Alexander', 'Rotterdam Centraal', 0], ['Rotterdam Centraal', 'Schiedam Centrum', 0],
+        ['Schiedam Centrum', 'Delft', 0], ['Delft', 'Den Haag Centraal', 0], ['Den Haag Centraal', 'Gouda', 0]]]
+
+    traject_random = [[['Rotterdam Alexander', 'Rotterdam Centraal', '8'], ['Rotterdam Centraal', 'Dordrecht', '17'],
+        ['Dordrecht', 'Rotterdam Centraal', '17'], ['Rotterdam Centraal', 'Rotterdam Alexander', '8'],
+        ['Rotterdam Alexander', 'Rotterdam Centraal', '8'], ['Rotterdam Centraal', 'Schiedam Centrum', '5'], ['Schiedam Centrum', 'Delft', '7'],
+        ['Delft', 'Den Haag Centraal', '13'], ['Den Haag Centraal', 'Leiden Centraal', '12'], ['Leiden Centraal', 'Den Haag Centraal', '12'],
+        ['Den Haag Centraal', 'Leiden Centraal', '12']], [['Zaandam', 'Castricum', '12'], ['Castricum', 'Zaandam', '12'], ['Zaandam', 'Hoorn', '26'],
+        ['Hoorn', 'Zaandam', '26'], ['Zaandam', 'Hoorn', '26']], [['Rotterdam Centraal', 'Schiedam Centrum', '5'],
+        ['Schiedam Centrum', 'Rotterdam Centraal', '5'], ['Rotterdam Centraal', 'Rotterdam Alexander', '8'], ['Rotterdam Alexander', 'Gouda', '10'],
+        ['Gouda', 'Den Haag Centraal', '18'], ['Den Haag Centraal', 'Gouda', '18'], ['Gouda', 'Alphen a/d Rijn', '19'], ['Alphen a/d Rijn', 'Gouda', '19'],
+        ['Gouda', 'Den Haag Centraal', '18']], [['Castricum', 'Beverwijk', '13'], ['Beverwijk', 'Zaandam', '25'], ['Zaandam', 'Beverwijk', '25'],
+        ['Beverwijk', 'Castricum', '13'], ['Castricum', 'Alkmaar', '9'], ['Alkmaar', 'Castricum', '9'], ['Castricum', 'Zaandam','12'],
+        ['Zaandam', 'Castricum', '12']], [['Den Helder', 'Alkmaar', '36'], ['Alkmaar', 'Castricum', '9'], ['Castricum', 'Beverwijk', '13'],
+        ['Beverwijk', 'Castricum', '13'], ['Castricum', 'Alkmaar', '9'], ['Alkmaar', 'Castricum', '9'], ['Castricum', 'Zaandam', '12'],
+        ['Zaandam', 'Castricum', '12']]]
+
+    traject_greedy = [[['Den Helder', 'Alkmaar', 36], ['Alkmaar', 'Castricum', 9], ['Castricum', 'Zaandam', 12], ['Zaandam', 'Amsterdam Sloterdijk', 6],
+        ['Amsterdam Sloterdijk', 'Amsterdam Centraal', 6], ['Amsterdam Centraal', 'Amsterdam Amstel', 8], ['Amsterdam Amstel', 'Amsterdam Zuid', 10],
+        ['Amsterdam Zuid', 'Schiphol Airport', 6], ['Schiphol Airport', 'Leiden Centraal', 15], ['Leiden Centraal', 'Den Haag Centraal', 12]],
+        [['Dordrecht', 'Rotterdam Centraal', 17], ['Rotterdam Centraal', 'Schiedam Centrum', 5], ['Schiedam Centrum', 'Delft', 7],
+        ['Delft', 'Den Haag Centraal', 13], ['Den Haag Centraal', 'Leiden Centraal', 12], ['Leiden Centraal', 'Heemstede-Aerdenhout', 13],
+        ['Heemstede-Aerdenhout', 'Haarlem', 6], ['Haarlem', 'Amsterdam Sloterdijk', 11], ['Amsterdam Sloterdijk', 'Amsterdam Centraal', 6],
+        ['Amsterdam Centraal', 'Amsterdam Amstel', 8], ['Amsterdam Amstel', 'Amsterdam Zuid', 10], ['Amsterdam Zuid', 'Schiphol Airport', 6],
+        ['Schiphol Airport', 'Amsterdam Zuid', 6]], [['Dordrecht', 'Rotterdam Centraal', 17], ['Rotterdam Centraal', 'Schiedam Centrum', 5],
+        ['Schiedam Centrum', 'Delft', 7], ['Delft', 'Den Haag Centraal', 13], ['Den Haag Centraal', 'Leiden Centraal', 12],
+        ['Leiden Centraal', 'Heemstede-Aerdenhout', 13], ['Heemstede-Aerdenhout', 'Haarlem', 6], ['Haarlem', 'Amsterdam Sloterdijk', 11],
+        ['Amsterdam Sloterdijk', 'Amsterdam Centraal', 6], ['Amsterdam Centraal', 'Amsterdam Amstel', 8], ['Amsterdam Amstel', 'Amsterdam Zuid', 10],
+        ['Amsterdam Zuid', 'Schiphol Airport', 6], ['Schiphol Airport', 'Amsterdam Zuid', 6]], [['Amsterdam Centraal', 'Amsterdam Sloterdijk', 6],
+        ['Amsterdam Sloterdijk', 'Zaandam', 6], ['Zaandam', 'Castricum', 12], ['Castricum', 'Alkmaar', 9], ['Alkmaar', 'Hoorn', 24],
+        ['Hoorn', 'Zaandam', 26], ['Zaandam', 'Amsterdam Sloterdijk', 6], ['Amsterdam Sloterdijk', 'Amsterdam Centraal', 6],
+        ['Amsterdam Centraal', 'Amsterdam Amstel', 8], ['Amsterdam Amstel', 'Amsterdam Zuid', 10], ['Amsterdam Zuid', 'Schiphol Airport', 6]],
+        [['Amsterdam Sloterdijk', 'Amsterdam Centraal', 6], ['Amsterdam Centraal', 'Amsterdam Amstel', 8], ['Amsterdam Amstel', 'Amsterdam Zuid', 10],
+        ['Amsterdam Zuid', 'Schiphol Airport', 6], ['Schiphol Airport', 'Leiden Centraal', 15], ['Leiden Centraal', 'Den Haag Centraal', 12],
+        ['Den Haag Centraal', 'Delft', 13], ['Delft', 'Schiedam Centrum', 7], ['Schiedam Centrum', 'Rotterdam Centraal', 5],
+        ['Rotterdam Centraal', 'Rotterdam Alexander', 8], ['Rotterdam Alexander', 'Gouda', 10], ['Gouda', 'Den Haag Centraal', 18]]]
+
     # TODO Make sure the file path is correct after importing!
     dict_stations_holland, dict_connections_holland = read_data('../StationsHolland.csv', '../ConnectiesHolland.csv')
     dict_stations_nl, dict_connections_nl = read_data('../StationsNationaal.csv', '../ConnectiesNationaal.csv')
@@ -243,3 +295,13 @@ if __name__ == "__main__":
     # calling function of complete visual product
     # visualize_all_trajects(dict_stations_holland, traject_long, province = True)
     visualize_all_trajects(dict_stations_nl, traject_long, province = False)
+
+    # exit () prevents last block from executing
+    exit()
+
+    visualize_all_trajects(dict_stations_holland, traject_depthfirst, province = True, save_figure = True,
+                            file_name = 'visualisation_depthfirst.png', algorithm = 'DepthFirst')
+    visualize_all_trajects(dict_stations_holland, traject_random, province = True, save_figure = True,
+                            file_name = 'visualisation_random.png', algorithm = 'Random')
+    visualize_all_trajects(dict_stations_holland, traject_greedy, province = True, save_figure = True,
+                            file_name = 'visualisation_greedy.png', algorithm = 'Greedy')
